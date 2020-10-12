01861f9baf7cd1968005da674a1625f530df5a1b
Hurly77 <cameron.leverett@yahoo.com>
2020-10-12 16:33:59 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import { render } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=> console.log("hello")} />
   </div>,
   document.getElementById('root')
 );
