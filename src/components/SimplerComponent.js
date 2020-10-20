dd693ecb8c697b5be3ee6af30a06ecec1c947599
StevenDRiggs <sfriggs21@gmail.com>
2020-10-20 05:30:51 -0600

    WIP; committed for device change; check tests for progress

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+
+const SimplerComponent = () => {
+    return (
+        <div>
+            I am just happy
+        </div>
+    )
+}
+
+export default SimplerComponent
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={event => event.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)} />
   </div>,
   document.getElementById('root')
 );
